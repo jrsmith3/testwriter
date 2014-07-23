72925280baf943e121db73d6503d54b6cd31a5ca
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2014-07-23 18:04:47 -0400

    Command-line code, UI. Method to read a slug from a file.

--- testwriter/testwriter.py
+++ testwriter/testwriter.py
@@ -1,4 +1,6 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import argparse
 
 def tests_generator(slug, names):
     """
@@ -20,7 +22,40 @@ def read_names(filename):
     """
     Returns names listed in specified file as list, stripped of whitespace.
     """
-    with open(filename,"r") as f:
+    with open(filename, "r") as f:
         names = [line.strip() for line in f]
 
     return names
+
+def read_slug(filename):
+    """
+    Returns the test slug found in specified filename.
+    """
+    with open(filename, "r") as f:
+        slug = f.read()
+
+    return slug
+
+class main():
+    """
+    Class that always gets instantiated when program is run
+    """
+
+    def __init__(self):
+
+        parser = argparse.ArgumentParser(description = "Easily generate similar unit test methods.")
+
+        parser.add_argument("slug",
+            help = "Path to file containing test slug.")
+        parser.add_argument("names",
+            help = "Path to file containing names to be substituted into test slug.")
+
+        args = parser.parse_args()
+
+        names = read_names(args.names)
+        slug = read_slug(args.slug)
+
+        print tests_generator(slug, names)
+
+if __name__ == "__main__":
+    main()
