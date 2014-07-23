56678e620cd59152592db185b9c442f99bf45af2
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2014-07-23 16:29:52 -0400

    distutils setup.py sketch.

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(name = "testwriter",
+      version = "0.0.1",
+      author = "Joshua Ryan Smith",
+      author_email = "joshua.r.smith@gmail.com",
+      packages = ["testwriter"],
+      url = "https://github.com/jrsmith3/testwriter",
+      description = "A package to automate writing test methods.",
+      classifiers = ["Programming Language :: Python",
+                     "License :: OSI Approved :: MIT License",
+                     "Operating System :: OS Independent",
+                     "Development Status :: 2 - Pre-Alpha",
+                     "Intended Audience :: Developers",
+                     "Topic :: Software Development :: Testing",
+                     "Natural Language :: English",],)
